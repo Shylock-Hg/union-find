34f7069dba13c272e484e57180499ac4ff3699aa
Shylock-Hg <tcath2s@gmail.com>
2018-12-27 07:25:59 +0000

    Optimize adding comments and large scale testing.

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,26 @@ use std::fmt;
 
 /// The union-find algorithm trait
 pub trait UnionFind {
-        /// Combine two union to one
-        /// param index1: index of one item
-        /// param index2: index of the other item
-        fn union(&mut self, index1: usize, index2: usize);
+        type K;
 
-        /// Check is two item in one union
-        /// param index1: index of one item
-        /// param index2: index of the other item
-        fn find(&self, index1: usize, index2: usize) -> bool;
+        /// union the union by index of vector
+        /// param k1: key of one item in union
+        /// param k2: key of one item in union
+        fn union(&mut self, k1: Self::K, k2: Self::K);
+
+        /// find is the two item in same union
+        /// param k1: key of one item in union
+        /// param k2: key of one item in union
+        /// retval: is the two items in same union
+        fn find(&self, k1: Self::K, k2: Self::K) -> bool;
 }
 
+/// UnionWeightedNode is the node of WQU union-find algorithm
+///     - parent: the parent index of current item, None for root node.
+///     - children: the children index of current item, Empty for leaf.
+///     - value: the value of the node.
 #[derive(Debug)]
+#[derive(PartialEq)]
 struct UnionWeightedNode<V: PartialEq + fmt::Debug> {
         parent: Option<usize>,  // index of parent, None for root
         children: Vec<usize>,  // index of child, Empty for leaf
@@ -32,18 +40,8 @@ impl<V: PartialEq + fmt::Debug> Default for UnionWeightedNode<V> {
         }
 }
 
-impl<V: PartialEq + fmt::Debug> PartialEq for UnionWeightedNode<V> {
-        fn eq(&self, other: &Self) -> bool {
-                self.parent == other.parent && self.children == other.children && self.value == other.value
-        }
-}
-
-/// UnionFind is the data structure of union-find algorithm
+/// UnionWeighted is the data structure of WQU union-find algorithm
 ///     Represent union by tree in vector.
-///     The parent vector store the parent Option<index> of current root.
-///     - Root root hold the Some(index) of itself.
-///     - Invalid root hold None.
-///     - Other root hold Some(parent index).
 #[derive(Debug)]
 pub struct UnionWeighted<V: PartialEq + fmt::Debug> {
         nodes: Vec<UnionWeightedNode<V>>,
@@ -59,14 +57,15 @@ impl<V: PartialEq + fmt::Debug> Default for UnionWeighted<V> {
 
 
 impl<V: PartialEq + fmt::Debug> UnionFind for UnionWeighted<V> {
+        type K = usize;
 
         /// union the union by index of vector
-        /// param index1: index of vector of one union
-        /// param index2: index of vector of one union
-        fn union(&mut self, index1: usize, index2: usize) {
-                // not well when index1 or index2 is lowset root
+        /// param k1: key of one item in union
+        /// param k2: key of one item in union
+        fn union(&mut self, k1: Self::K, k2: Self::K) {
+                // not well when k1 or k2 is lowset root
                 let ((root1, depth1), (root2, depth2)) =
-                        (self.get_root_with_depth(index1), self.get_root_with_depth(index2));
+                        (self.get_root_with_depth(k1), self.get_root_with_depth(k2));
                 if depth1 < depth2 {  // link shorter tree follow to longer
                         self.nodes[root1].parent = Some(root2);
                         self.nodes[root2].children.push(root1);
@@ -77,11 +76,11 @@ impl<V: PartialEq + fmt::Debug> UnionFind for UnionWeighted<V> {
         }
 
         /// find is the two item in same union
-        /// param index1: index of vector of one union
-        /// param index2: index of vector of one union
+        /// param k1: key of one item in union
+        /// param k2: key of one item in union
         /// retval: is the two items in same union
-        fn find(&self, index1: usize, index2: usize) -> bool {
-                let (root1, root2) = (self.get_root(index1), self.get_root(index2));
+        fn find(&self, k1: Self::K, k2: Self::K) -> bool {
+                let (root1, root2) = (self.get_root(k1), self.get_root(k2));
                 if root1 == root2 {
                         true
                 } else {
@@ -106,7 +105,7 @@ impl<V: PartialEq + fmt::Debug> UnionWeighted<V> {
 
         /// create a UnionWeighted
         /// param count: the capacity of union
-        fn new(count: usize) -> Self {
+        pub fn new(count: usize) -> Self {
                 UnionWeighted {
                         nodes: Vec::with_capacity(count),
                 }
@@ -114,7 +113,7 @@ impl<V: PartialEq + fmt::Debug> UnionWeighted<V> {
 
         /// create a UnionWeighted with category
         /// param count: the capacity of union
-        fn new_full_category(count: usize) -> Self {
+        pub fn new_full_category(count: usize) -> Self {
                 UnionWeighted {
                         nodes: full!(count),
                 }
@@ -123,7 +122,7 @@ impl<V: PartialEq + fmt::Debug> UnionWeighted<V> {
         /// get the root of one union
         /// param index: the index of current id value in vector
         /// retval: Option<index of root>
-        fn get_root(&self, index: usize) -> usize {
+        pub fn get_root(&self, index: usize) -> usize {
                 if let Some(p) = self.nodes[index].parent {
                         self.get_root(p)
                 } else {
@@ -146,7 +145,7 @@ impl<V: PartialEq + fmt::Debug> UnionWeighted<V> {
         /// get the root and the depth of tree
         /// param index: the index of current id value in vector
         /// retval: (Option<index of root>, depth)
-        fn get_root_with_depth(&self, index: usize) -> (usize, usize) {
+        pub fn get_root_with_depth(&self, index: usize) -> (usize, usize) {
                 let root = self.get_root(index);
                 let depth = self.get_depth_4_root(&self.nodes[index], 0);
                 (root, depth)
@@ -191,5 +190,31 @@ mod tests {
 
                 u.union(0, 3);
                 assert!(u.find(0, 2));
+        }
+
+        #[test]
+        fn larget_test() {
+                const COUNT: usize = 1024*1024*32;
+                use super::UnionFind;
+                let mut u: super::UnionWeighted<i32> = super::UnionWeighted::new_full_category(COUNT);
+                for i in 0..COUNT {
+                        assert_eq!(i, u.get_root(i));
+                        assert_eq!((i, 0), u.get_root_with_depth(i));
+                }
+                for i in 0..COUNT-1 {
+                        assert_eq!(false, u.find(i, i+1));
+                }
+
+                // union all categories
+                for i in 0..COUNT-1 {  // union all categories
+                        u.union(i, i+1);
+                }
+                for i in 0..COUNT-1 {  // check
+                        assert_eq!(true, u.find(i, i+1));
+                }
+                for i in 0..COUNT {  // flat check
+                        let (_, depth) = u.get_root_with_depth(i);
+                        assert!(depth < 3);
+                }
         }
 }
