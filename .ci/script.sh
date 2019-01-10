b4ab6ef67e48f3b6873bf1bc54c61b75856151d6
Shylock-Hg <tcath2s@gmail.com>
2019-01-10 03:10:27 +0000

    Naive travis-ci pipe line script.

---
+++ .ci/script.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env sh
+
+###############################################################################
+#!  \brief  
+#   \author Shylock Hg
+#   \date 2019-01-10
+#   \email tcath2s@gmail.com
+###############################################################################
+
+# build, test and generate docs in this phase
+
+set -ex
+
+. "$(dirname $0)/utils.sh"
+
+main() {
+    # Test a normal debug build.
+    if is_arm; then
+        cargo build --target "$TARGET" --verbose
+    else
+        cargo build --target "$TARGET" --verbose --all --features 'pcre2'
+    fi
+
+    # Show the output of the most recent build.rs stderr.
+    set +x
+    stderr="$(find "target/$TARGET/debug" -name stderr -print0 | xargs -0 ls -t | head -n1)"
+    if [ -s "$stderr" ]; then
+      echo "===== $stderr ====="
+      cat "$stderr"
+      echo "====="
+    fi
+    set -x
+
+    # Apparently tests don't work on arm, so just bail now. I guess we provide
+    # ARM releases on a best effort basis?
+    if is_arm; then
+      return 0
+    fi
+
+    # Run tests for ripgrep and all sub-crates.
+    cargo test --target "$TARGET" --verbose --all --features 'pcre2'
+}
+
+main
